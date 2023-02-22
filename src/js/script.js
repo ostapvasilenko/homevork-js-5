71c5c8a080ecb0b8bcf339ca9f339b12463f959b
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-02-22 12:52:24 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
-function openPage(pageName) {
-  var i, pages;
-  pages = document.getElementsByClassName("page");
-  for (i = 0; i < pages.length; i++) {
-    pages[i].style.display = "none";
-  }
-  document.getElementById(pageName).style.display = "block";
+const modal = document.getElementById("modal");
+const openModalBtn = document.getElementById("open-modal-btn");
+const closeModalBtn = document.getElementsByClassName("close")[0];
+
+function openModal() {
+  modal.style.display = "block";
 }
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+openModalBtn.addEventListener("click", openModal);
+
+closeModalBtn.addEventListener("click", closeModal);
